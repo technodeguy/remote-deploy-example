c6564700473599f1c377e6a98879a3378b567c7e
Yuriy C <chernyak.yurii@wetelo.com>
2020-12-10 13:18:30 +0200

    6094 Fix global config

--- deploy/global_config.sh
+++ deploy/global_config.sh
@@ -16,6 +16,7 @@ server_home=/home/$server_user
 service_home=/home/$server_user/public_html/is/${server}/$service_name
 
 git_url=git@gitlab.com:wetelo-interim-sport/${service_name}.git
+pm2_exec_path=/home/$server_user/.nvm/versions/node/v12.19.0/bin/pm2
 
 if [[ $server != "staging" ]] && [[ $server != "production" ]]; then
     echo "Invalid server";
@@ -29,11 +30,14 @@ elif [[ "$server" == "production" ]]; then
 fi
 
 if [[ $service_name == "interim-sport-spa" ]]; then 
-    command="NODE_ENV=production next build && pm2 reload $pm2_prefix-is-ssr";
+    command="NODE_ENV=production next build && $pm2_exec_path reload $pm2_prefix-is-ssr";
 elif [[ $service_name == "interim-sport-api" ]]; then
-    command="pm2 reload $pm2_prefix-is-api";
+    command="$pm2_exec_path reload $pm2_prefix-is-api";
 elif [[ $service_name == "interim-sport-root-spa" ]]; then
     command="npm run build";
 elif [[ $service_name == "interim-sport-root-api" ]]; then
-    command="pm2 reload $pm2_prefix-is-root-api";
+    command="$pm2_exec_path reload $pm2_prefix-is-root-api";
+else 
+    echo "Invalid service name";
+    exit 1;
 fi
